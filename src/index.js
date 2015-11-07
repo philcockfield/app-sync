75d6a7fd987e989c858ea208312b955e11c4e0fe
Phil Cockfield <phil@cockfield.net>
2015-11-07 13:41:37 +1300

    Refactored the way initial configuration data is pulled into the synced.

--- src/index.js
+++ src/index.js
@@ -2,64 +2,71 @@ import R from "ramda";
 import fsPath from "path";
 
 const DEFAULT_PORT = 5000;
-let targetFolder = "./sync";
+let githubSettings = {};
+
 const isEmpty = (value) => (R.isNil(value) || R.isEmpty(value));
 
 
-const api = {
-  apps: [],
 
 
-  /**
-   * Resets the state of the API.
-   */
-  reset() {
-    this.apps = [];
-  },
 
 
-  /**
-   * Gets or sets the folder where apps are synced to.
-   * @param {String} value: Optional - the path value to set.
-   * @return {String} path if not setting the value.
-   *         {Object} this API if setting the value (allows for chained API).
-   */
-  targetFolder(value) {
-    if (value === undefined) {
-      // READ.
-      return fsPath.resolve(targetFolder);
-    } else {
-      // WRITE.
-      targetFolder = value;
+// export default api;
+
+/**
+ * Initializes a new app syncer.
+ * @param settings:
+ *          - userAgent:    https://developer.github.com/v3/#user-agent-required
+ *          - targetFolder: The root location where apps are downloaded to.
+ *          - token:        The Github authorization token to use for calls to
+ *                          restricted resources.
+ *                             see: https://github.com/settings/tokens
+ */
+export default (settings = {}) => {
+  const userAgent = settings.userAgent || "app-syncer";
+  const targetFolder = settings.targetFolder || "./.synced-apps"
+
+  return {
+    apps: [],
+    userAgent,
+    targetFolder,
+    token: settings.token,
+
+
+
+    /**
+     * Adds a new application to run.
+     * @param {string} name: The unique name of the app (ID).
+     * @param {repo}  repo:  The Github 'username/repo'.
+     *                       Optionally you can specify a sub-path within the repos
+     *                       like this:
+     *                            'username/repo/my/sub/path'
+     */
+    add(name, repo, options = {}) {
+      // Setup initial conditions.
+      if (isEmpty(name)) { throw new Error("'name' of app required"); }
+      if (isEmpty(repo)) { throw new Error("'repo' name required, eg. 'username/my-repo'"); }
+      if (R.find(item => item.name === name, this.apps)) {
+        throw new Error(`An app with the name '${ name }' has already been registered.`);
+      }
+
+      // Extract the repo and sub-path.
+      const parts = repo.split("/");
+      if (parts.length < 2) { throw new Error(`A repo must have a 'user-name' and 'repo-name', eg 'username/repo'.`); }
+      repo = `${ parts[0] }/${ parts[1] }`;
+      const path = R.takeLast(parts.length - 2, parts).join("/");
+
+
+      // Store values.
+      const item = R.clone(options);
+      item.name = name;
+      item.repo = repo;
+      item.port = DEFAULT_PORT + (this.apps.length);
+      if (!isEmpty(path)) { item.path = path; }
+      this.apps.push(item);
+
+      // Finish up.
       return this;
     }
-  },
-
-
-  /**
-   * Adds a new application to run.
-   */
-  add(name, repo, options = {}) {
-    // Setup initial conditions.
-    if (isEmpty(name)) { throw new Error("'name' of app required"); }
-    if (isEmpty(repo)) { throw new Error("'repo' name required, eg. 'username/my-repo'"); }
-    if (R.find(item => item.name === name, this.apps)) {
-      throw new Error(`An app with the name '${ name }' has already been registered.`);
-    }
-
-    // Store values.
-    const item = R.clone(options);
-    item.name = name;
-    item.repo = repo;
-    item.port = DEFAULT_PORT + (this.apps.length);
-    api.apps.push(item);
-
-    // Finish up.
-    return this;
-  }
+  };
 };
-
-
-
-
-export default api;
--- test/api.test.js
+++ test/api.test.js
@@ -3,57 +3,84 @@ import { expect } from "chai";
 import syncer from "../src";
 import fsPath from "path";
 
+// NOTE: Tests will work if the GITHUB_TOKEN is not present.
+//       The rate-limit will be lower though, so when testing locally
+//       if you run into a rate-limit problem add a token to your bash script.
+//
+//          https://github.com/settings/tokens
+//
+const GITHUB_TOKEN = process.env.GITHUB_TOKEN
+
 
 
 describe("Main API", () => {
-  describe("targetFolder", function() {
-    it("returns the default 'targetFolder' path", () => {
-      expect(syncer.targetFolder()).to.equal(fsPath.resolve("./sync"));
+  describe("init", function() {
+    it("initializes with default values", () => {
+      const app = syncer();
+      expect(app.userAgent).to.equal("app-syncer");
     });
 
-    it("changes the 'targetFolder' path", () => {
-      syncer
-        .targetFolder("./foo")
-        .targetFolder("./bar")
-      expect(syncer.targetFolder()).to.equal(fsPath.resolve("./bar"));
+    it("has default values", () => {
+      const app = syncer();
+      expect(app.apps).to.eql([]);
+      expect(app.targetFolder).to.equal("./.synced-apps");
+    });
+
+    it("stores optional values", () => {
+      const app = syncer({
+        token: "my-github-token"
+      });
+      expect(app.token).to.equal("my-github-token");
     });
   });
 
-  describe("add", function() {
 
+  describe("add", function() {
+    let app;
     beforeEach(() => {
-      syncer.reset();
+      app = syncer({ token: GITHUB_TOKEN });
     });
 
     it("has no apps", () => {
-      expect(syncer.apps).to.eql([]);
+      expect(app.apps).to.eql([]);
     });
 
-    it("adds an app", () => {
-      syncer.add("my-app", "philcockfield/node-syncer", { path: "/example/app" });
-      expect(syncer.apps[0].name).to.equal("my-app");
-      expect(syncer.apps[0].repo).to.equal("philcockfield/node-syncer");
-      expect(syncer.apps[0].path).to.equal("/example/app");
+    it("adds an app (root of repo)", () => {
+      app.add("my-app", "philcockfield/node-syncer");
+      expect(app.apps[0].name).to.equal("my-app");
+      expect(app.apps[0].repo).to.equal("philcockfield/node-syncer");
+      expect(app.apps[0].path).to.equal(undefined);
+    });
+
+    it("adds an app with a path to a sub-folder within the repo", () => {
+      app.add("my-app", "philcockfield/node-syncer/example/app-1");
+      expect(app.apps[0].name).to.equal("my-app");
+      expect(app.apps[0].repo).to.equal("philcockfield/node-syncer");
+      expect(app.apps[0].path).to.equal("example/app-1");
     });
 
     it("throws if the 'name' or 'repo' are not specified", () => {
-      expect(() => syncer.add(undefined, "my-repo")).to.throw();
-      expect(() => syncer.add("name")).to.throw();
+      expect(() => app.add(undefined, "user/my-repo")).to.throw();
+      expect(() => app.add("name")).to.throw();
+    });
+
+    it("throws if the repo is not two parts (username/repo-name)", () => {
+      expect(() => app.add("my-app", "fail")).to.throw();
     });
 
     it("throws if a 'name' is repeated", () => {
-      syncer.add("my-app", "my-repo-1");
+      app.add("my-app", "user/my-repo-1");
       let fn = () => {
-        syncer.add("my-app", "my-repo-2");
+        app.add("my-app", "user/my-repo-2");
       };
       expect(fn).to.throw();
     });
 
     it("auto-assigns port numbers", () => {
-      syncer.add("my-app-1", "my-repo");
-      syncer.add("my-app-2", "my-repo");
-      expect(syncer.apps[0].port).to.equal(5000);
-      expect(syncer.apps[1].port).to.equal(5001);
+      app.add("my-app-1", "user/my-repo");
+      app.add("my-app-2", "user/my-repo");
+      expect(app.apps[0].port).to.equal(5000);
+      expect(app.apps[1].port).to.equal(5001);
     });
   });
 });
