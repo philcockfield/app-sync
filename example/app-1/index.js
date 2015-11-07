e6fa793f335611408098e415d023cd93ad578578
Phil Cockfield <phil@cockfield.net>
2015-11-07 17:11:55 +1300

    Express app example

--- example/app-1/index.js
+++ example/app-1/index.js
@@ -2,3 +2,17 @@
 console.log("Sample App");
 console.log("process.argv", process.argv);
 console.log("");
+
+var express = require("express");
+var app = express();
+
+
+app.get("/", function(req, res) {
+  res.send("Hello World!");
+});
+
+
+const PORT = 5000;
+app.listen(PORT, function() {
+  console.log("PORT", PORT);
+})
