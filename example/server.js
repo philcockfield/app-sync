608ef549e3729743003f3f6f99f90c88eb47ff85
Phil Cockfield <phil@cockfield.net>
2015-11-26 15:19:03 +1300

    Test

--- example/server.js
+++ example/server.js
@@ -7,8 +7,8 @@ const gateway = appSync({
 
 gateway
   // .add("ui-harness", "philcockfield/ui-harness-site", "*")
-  .add("one", "philcockfield/app-sync/example/app-2", "*", { branch: "devel" })
-  .add("two", "philcockfield/app-sync/example/app-1", "*/1", { branch: "devel" });
+  .add("two", "philcockfield/app-sync/example/app-2", "*", { branch: "devel" })
+  .add("one", "philcockfield/app-sync/example/app-1", "*/1", { branch: "devel" });
 
 
 
