425f70732a37265b288ece56489aaf6f89216ccf
Phil Cockfield <phil@cockfield.net>
2015-11-29 12:06:36 +1300

    Stubbed in test and file.

---
+++ test/integration/manifest.intergration-test.js
@@ -0,0 +1,11 @@
+"use strict";
+import { expect } from "chai";
+import fsPath from "path";
+import fs from "fs-extra";
+import manifest from "../../src/manifest";
+
+
+
+describe.only("manifest (integration)", function() {
+
+});
