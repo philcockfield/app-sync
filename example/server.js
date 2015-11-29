c4da0636636d52728b7bf8488c5b89718b8d88bf
Phil Cockfield <phil@cockfield.net>
2015-11-29 14:53:33 +1300

    Invoking with manifest

--- example/server.js
+++ example/server.js
@@ -2,13 +2,14 @@ import appSync from "../src/main";
 
 const gateway = appSync({
   token: process.env.GITHUB_TOKEN,
-  apiRoute: "*/api"
+  apiRoute: "*/api",
+  manifest: "philcockfield/app-sync/example/manifest.yml:devel"
 });
 
-gateway
-  // .add("ui-harness", "philcockfield/ui-harness-site", "*")
-  .add("two", "philcockfield/app-sync/example/app-2", "*", { branch: "devel" })
-  .add("one", "philcockfield/app-sync/example/app-1", "*/1", { branch: "devel" });
+// gateway
+//   // .add("ui-harness", "philcockfield/ui-harness-site", "*")
+//   .add("two", "philcockfield/app-sync/example/app-2", "*", { branch: "devel" })
+//   .add("one", "philcockfield/app-sync/example/app-1", "*/1", { branch: "devel" });
 
 
 
