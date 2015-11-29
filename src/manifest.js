db64a38dbab3cfd9be2d1a798a17b7ac04a4c2bd
Phil Cockfield <phil@cockfield.net>
2015-11-29 14:31:53 +1300

    Downloading manifest YAML from repo.

--- src/manifest.js
+++ src/manifest.js
@@ -1,6 +1,38 @@
+import Promise from "bluebird";
+import yaml from "js-yaml";
 
 
+/**
+ * Retrieves a manifest file from a remote repo.
+ * @param {string} repoPath: The path to the `repo/file.yml:branch` to retrieve.
+ * @return {Promise}
+ */
+export const getManifest = (repo, repoPath) => {
+  return new Promise((resolve, reject) => {
+    Promise.coroutine(function*() {
+      // Extract the path.
+      const parts = repoPath.trim().split(":");
+      const path = parts[0].trim();
+      const branch = (parts[1] || "master").trim();
 
-export default () => {
+      // Ensure a YAML file was specified.
+      if (!path.endsWith(".yml")) {
+        return reject(new Error("A path to a YAML file must be specified (.yml)"));
+      }
 
+      // Pull file from the repo.
+      const download = yield repo.get(path, { branch }).catch(err => reject(err));
+      const files = download && download.files;
+
+      // Parse and return the manifest.
+      if (files && files.length > 0) {
+        const manifest = files[0].toString();
+        try {
+          resolve(yaml.safeLoad(manifest));
+        } catch (e) {
+          reject(new Error(`Failed while parsing YAML: ${ e.message }`));
+        }
+      }
+    })();
+  });
 };
--- test/integration/manifest.intergration-test.js
+++ test/integration/manifest.intergration-test.js
@@ -2,10 +2,57 @@
 import { expect } from "chai";
 import fsPath from "path";
 import fs from "fs-extra";
-import manifest from "../../src/manifest";
+import github from "file-system-github";
+import { getManifest } from "../../src/manifest";
+
+// NOTE: Tests will work if the GITHUB_TOKEN is not present.
+//       The rate-limit will be lower though, so when testing locally
+//       if you run into a rate-limit problem add a token to your bash script.
+//
+//          https://github.com/settings/tokens
+//
+const GITHUB_TOKEN = process.env.GITHUB_TOKEN
+const BRANCH = "manifest"; // TODO: Change branch to "devel"
 
 
 
 describe.only("manifest (integration)", function() {
+  const repo = github.repo("test", "philcockfield/app-sync", { token: GITHUB_TOKEN });
 
+  describe("getManifest", function() {
+    it("retrieves a `manifest.yml`", () => {
+      return getManifest(repo, `/example/manifest.yml:${ BRANCH }`)
+        .then(result => {
+          expect(result.apps).to.be.an.instanceof(Array);
+        });
+    });
+
+    it("fails if a YAML file was not specified", (done) => {
+      getManifest(repo, "/example")
+        .catch(err => {
+          expect(err.message).to.equal("A path to a YAML file must be specified (.yml)");
+          done()
+        });
+    });
+
+    it("fails to retrieve a manifest (404)", (done) => {
+      getManifest(repo, "/does/not/exist.yml")
+        .catch(err => {
+          expect(err.status).to.equal(404);
+          done()
+        });
+    });
+
+    it("fails to parse YAML", (done) => {
+      getManifest(repo, `/example/manifest-corrupt.yml:${ BRANCH }`)
+        .then(result => {
+          console.log("result", result);
+        })
+        .catch(err => {
+          expect(err.message.startsWith("Failed while parsing YAML")).to.equal(true);
+          done()
+        });
+    });
+
+  });
 });
