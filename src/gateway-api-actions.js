6b73a9cd85d977f0e06617e5eb248d99724c8bdd
Phil Cockfield <phil@cockfield.net>
2015-12-01 19:13:27 +1300

    Stubbed in REST actions API module.

---
+++ src/gateway-api-actions.js
@@ -0,0 +1,13 @@
+import R from "ramda";
+import log from "./log";
+
+
+
+export default (apps, manifest) => {
+  return {
+    restart(req, res) {
+      console.log("req.params", req.params);
+      res.send("restart");
+    }
+  };
+};
